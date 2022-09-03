40b55cad59feb1b810b39103274978c15b0f57ad
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-09-03 14:14:44 +0900

    v0.0.0

---
+++ fall0903/src/main/java/com/spring/mydiv/Fall0903Application.java
@@ -0,0 +1,13 @@
+package com.spring.mydiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fall0903Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fall0903Application.class, args);
+	}
+
+}
---
+++ fall0903/src/test/java/com/spring/mydiv/Fall0903ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mydiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fall0903ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
